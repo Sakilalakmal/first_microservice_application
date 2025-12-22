3e7673fcab54f9ad951b9006aaf60c4738f989c3
Ganwari Nakathige Sakila Lakmal Nandasiri CB014383 <164737611+Sakilalakmal@users.noreply.github.com>
2025-12-22 21:55:31 +0530

    add Comment creation and listing functionality to PostList component

---
+++ client/src/PostList.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import CommentCreat from "./CommentCreat";
+import CommentList from "./CommentList";
+
+function PostList() {
+  const [posts, setPosts] = useState({});
+
+  const fethcPosts = async () => {
+    const response = await fetch("http://localhost:3000/posts");
+    const data = await response.json();
+    setPosts(data);
+  };
+
+  useEffect(() => {
+    fethcPosts();
+  }, []);
+
+  console.log(posts);
+
+  const renderPosts = Object.values(posts).map((post) => {
+    return (
+      <div key={post.id} className="card">
+        <div className="card-body">
+          <h3>{post.title}</h3>
+          <CommentList postId={post.id} />
+        </div>
+        <div className="px-2 py-2">
+          <CommentCreat postId={post.id} />
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderPosts}
+    </div>
+  );
+}
+
+export default PostList;
