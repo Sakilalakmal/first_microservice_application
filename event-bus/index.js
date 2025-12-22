6dcf078c6d89c573ab2cab840244c135f2b18143
Ganwari Nakathige Sakila Lakmal Nandasiri CB014383 <164737611+Sakilalakmal@users.noreply.github.com>
2025-12-22 22:47:38 +0530

    add event bus and query service for handling post and comment events

---
+++ event-bus/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+const PORT = 4005;
+
+app.post("/events", async (req, res) => {
+  const event = req.body;
+  axios.post("http://localhost:3000/events", event).catch((err) => {
+    console.log("Error forwarding event to posts service:", err.message);
+  });
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log("Error forwarding event to comments service:", err.message);
+  });
+  axios.post("http://localhost:6005/events", event).catch((err) => {
+    console.log("Error forwarding event to query service:", err.message);
+  });
+
+  res.send({ status: "OK" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Event Bus listening on port ${PORT}`);
+});
